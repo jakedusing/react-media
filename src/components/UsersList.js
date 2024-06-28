4f6660eb2bf87c6c742340b838e244da90cf3a0a
Jake Dusing <dusingjake@gmail.com>
2024-06-28 06:19:44 -0500

    add isLoadingUsers and loadingUsersError state

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,28 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
 
 function UsersList() {
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUsersError, setLoadingUsersError] = useState(null);
   const dispatch = useDispatch();
-  const { isLoading, data, error } = useSelector((state) => {
-    return state.users; // { data: [], isLoading: false, error: null }
+  const { data } = useSelector((state) => {
+    return state.users;
   });
 
   useEffect(() => {
-    dispatch(fetchUsers());
+    setIsLoadingUsers(true);
+    dispatch(fetchUsers())
+      .unwrap()
+      .then(() => {
+        console.log("success");
+      })
+      .catch(() => {
+        console.log("fail");
+      });
   }, [dispatch]); // dispatch is not really needed in this array, just there to make warning go away.  Array can be empty and it'd still work
 
   const handleUserAdd = () => {
     dispatch(addUser());
   };
 
-  if (isLoading) {
+  if (isLoadingUsers) {
     return <Skeleton times={6} className={"h-10 w-full"} />;
   }
 
-  if (error) {
+  if (loadingUsersError) {
     return <div>Error fetching data...</div>;
   }
 
