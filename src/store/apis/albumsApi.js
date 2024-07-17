641e387d09ef7e19ffc075aed472c28dbe27f032
Jake Dusing <dusingjake@gmail.com>
2024-07-17 18:39:19 -0500

    start remove implementation

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -18,6 +18,14 @@ const albumsApi = createApi({
   }),
   endpoints(builder) {
     return {
+      removeAlbum: builder.mutation({
+        query: (album) => {
+          return {
+            url: `/albums/${album.id}`,
+            method: "DELETE",
+          };
+        },
+      }),
       addAlbum: builder.mutation({
         invalidatesTags: (result, error, user) => {
           return [{ type: "Album", id: user.id }];
@@ -51,5 +59,9 @@ const albumsApi = createApi({
   },
 });
 
-export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
+export const {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useRemoveAlbumMutation,
+} = albumsApi;
 export { albumsApi };
